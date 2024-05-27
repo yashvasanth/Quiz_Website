b4c42c5bebb7bb784b984fd638581612544943c9
yashvasanth <139197282+yashvasanth@users.noreply.github.com>
2024-05-27 21:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,212 @@
+// const startButton = document.getElementById('start-btn');
+// const nextButton = document.getElementById('next-btn');
+// const questionContainerElement = document.getElementById('question-container');
+// const questionElement = document.getElementById('question');
+// const answerButtonsElement = document.getElementById('answer-buttons');
+
+// let shuffledQuestions, currentQuestionIndex;
+// let score = 0;
+
+// startButton.addEventListener('click', startGame);
+// nextButton.addEventListener('click', () => {
+//   currentQuestionIndex++;
+//   setNextQuestion();
+// });
+
+// function startGame() {
+//   startButton.classList.add('hide');
+//   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+//   currentQuestionIndex = 0;
+//   questionContainerElement.classList.remove('hide');
+//   setNextQuestion();
+// }
+
+// function setNextQuestion() {
+//   resetState();
+//   showQuestion(shuffledQuestions[currentQuestionIndex]);
+// }
+
+// function showQuestion(question) {
+//   questionElement.innerText = question.question;
+//   question.answers.forEach(answer => {
+//     const button = document.createElement('button');
+//     button.innerText = answer.text;
+//     button.classList.add('btn');
+//     if (answer.correct) {
+//       button.dataset.correct = answer.correct;
+//     }
+//     button.addEventListener('click', selectAnswer);
+//     answerButtonsElement.appendChild(button);
+//   });
+// }
+
+// function resetState() {
+//   clearStatusClass(document.body);
+//   nextButton.classList.add('hide');
+//   while (answerButtonsElement.firstChild) {
+//     answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+//   }
+// }
+
+// function selectAnswer(e) {
+//   const selectedButton = e.target;
+//   const correct = selectedButton.dataset.correct;
+//   setStatusClass(document.body, correct);
+//   Array.from(answerButtonsElement.children).forEach(button => {
+//     setStatusClass(button, button.dataset.correct);
+//   });
+//   if (shuffledQuestions.length > currentQuestionIndex + 1) {
+//     nextButton.classList.remove('hide');
+//   } else {
+//     startButton.innerText = 'Restart';
+//     startButton.classList.remove('hide');
+//   }
+// }
+
+// function setStatusClass(element, correct) {
+//   clearStatusClass(element);
+//   if (correct) {
+//     element.classList.add('correct');
+//   } else {
+//     element.classList.add('wrong');
+//   }
+// }
+
+// function clearStatusClass(element) {
+//   element.classList.remove('correct');
+//   element.classList.remove('wrong');
+// }
+
+// const questions = [
+//   {
+//     question: 'What is 2 + 2?',
+//     answers: [
+//       { text: '4', correct: true },
+//       { text: '22', correct: false },
+//       { text: '3', correct: false },
+//       { text: '5', correct: false }
+//     ]
+//   },
+//   {
+//     question: 'What is the capital of France?',
+//     answers: [
+//       { text: 'Paris', correct: true },
+//       { text: 'London', correct: false },
+//       { text: 'Berlin', correct: false },
+//       { text: 'Madrid', correct: false }
+//     ]
+//   }
+// ];
+
+
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const feedbackElement = document.getElementById('feedback');
+
+let shuffledQuestions, currentQuestionIndex;
+let score = 0;
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  feedbackElement.classList.add('hide');
+  feedbackElement.innerText = '';
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct === "true";
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct === "true");
+  });
+  if (correct) {
+    feedbackElement.innerText = 'Correct Answer!';
+    feedbackElement.classList.add('correct');
+  } else {
+    feedbackElement.innerText = 'Wrong Answer!';
+    feedbackElement.classList.add('wrong');
+  }
+  feedbackElement.classList.remove('hide');
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide');
+  } else {
+    startButton.innerText = 'Restart';
+    startButton.classList.remove('hide');
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+const questions = [
+  {
+    question: 'What is 2 + 2?',
+    answers: [
+      { text: '4', correct: true },
+      { text: '22', correct: false },
+      { text: '3', correct: false },
+      { text: '5', correct: false }
+    ]
+  },
+  {
+    question: 'What is the capital of France?',
+    answers: [
+      { text: 'Paris', correct: true },
+      { text: 'London', correct: false },
+      { text: 'Berlin', correct: false },
+      { text: 'Madrid', correct: false }
+    ]
+  }
+];
+
